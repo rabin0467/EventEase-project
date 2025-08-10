5684d27e2f3d78866d5d21ca60ab76f51bb25915
Rabin <m.rabinmia467@gmail.com>
2025-08-10 22:54:43 +0600

    database problem solved

--- event_management/settings.py
+++ event_management/settings.py
@@ -87,24 +87,24 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME', default=''),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', cast=int)
-    }
-}
-
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default='postgresql://event_management_db_dup8_user:VHmCFsOjoCQn6ssRteItfF3Iwk1XwulV@dpg-d1vmrtfgi27c738e0b40-a.oregon-postgres.render.com/event_management_db_dup8',
-#         conn_max_age=600
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': config('DB_NAME', default=''),
+#         'USER': config('DB_USER'),
+#         'PASSWORD': config('DB_PASSWORD'),
+#         'HOST': config('DB_HOST', default='localhost'),
+#         'PORT': config('DB_PORT', cast=int)
+#     }
 # }
+
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://event_management_db_dup8_user:VHmCFsOjoCQn6ssRteItfF3Iwk1XwulV@dpg-d1vmrtfgi27c738e0b40-a.oregon-postgres.render.com/event_management_db_dup8',
+        conn_max_age=600
+    )
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
